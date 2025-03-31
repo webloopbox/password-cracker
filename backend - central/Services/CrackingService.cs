49d12e07eb0427d3cb18a89deb09636339508715
Maksymilian Sowula <50271575+miluski@users.noreply.github.com>
2025-03-31 11:22:12 +0200

    minor: Changed mock commnicate informing that cracking was successfull or not

--- backend - central/Services/CrackingService.cs
+++ backend - central/Services/CrackingService.cs
@@ -7,15 +7,15 @@ namespace backend___central.Services
         public IResult HandleBruteForceCracking(HttpContext httpContext)
         {
             ILogService.LogInfo(logServices, "Made request to crack password using brute force method");
-            ILogService.LogInfo(logServices, "Final cracking time with brute force method was: 03:34:10");
-            return Results.Ok("Started brute force password cracking.");
+            ILogService.LogInfo(logServices, "Final cracking time with brute force method was: 03:34:10. Cracking was unsuccessfull.");
+            return Results.Accepted("Started brute force password cracking.");
         }
 
         public IResult HandleDictionaryCracking(HttpContext httpContext)
         {
             ILogService.LogInfo(logServices, "Made request to crack password using dictionary method");
-            ILogService.LogInfo(logServices, "Final cracking time with dictionary method was: 01:34:10");
-            return Results.Ok("Started dictionary password cracking.");
+            ILogService.LogInfo(logServices, "Final cracking time with dictionary method was: 01:34:10. Cracking was successfull.");
+            return Results.Accepted("Started dictionary password cracking.");
         }
     }
 }
